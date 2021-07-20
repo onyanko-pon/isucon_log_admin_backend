73e0ce9e73713cbf16c60907168f987a40247a8b
tsukasa-forever <engineer.tsukasa@gmail.com>
2021-07-20 23:39:57 +0900

    Setup express

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,30 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+if (process.env.NODE_ENV !== "production") {
+    var dotenv = require('dotenv');
+    dotenv.config();
+}
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+var PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
+// TODO 消す
+app.get('/hello', function (req, res) {
+    res.json({
+        message: "hello"
+    });
+});
+app.listen(PORT, function () {
+    console.log("Start on port " + PORT);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("hello")
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = require('dotenv')
+  dotenv.config()
+}
+
+import express from 'express'
+const app: express.Express = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+const PORT = process.env.PORT ?? 3000
+
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*")
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+})
+
+// TODO 消す
+app.get('/hello', (req: express.Request, res: express.Response) => {
+  res.json({
+    message: "hello"
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Start on port ${PORT}`)
+})
