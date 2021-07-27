d836203d9be40b5dade98a6a5bc359372dac25d1
tsukasa <54364185+onyanko-pon@users.noreply.github.com>
2021-07-27 18:24:22 +0900

    Update src/api/index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -16,7 +16,9 @@ router.get('/logs/:id', async (req: express.Request, res: express.Response) => {
   const logId = req.params.id
   try {
     const log = await Log.findByPk(logId)
-    if (log === null) return res.status(404).json(`id=${logId}のlogは存在しません`)
+    if (log === null) {
+      return res.status(404).json({message: `id=${logId}のlogは存在しません`})
+    }
     res.json({ log })
   } catch (e) {
     res.status(500).json({message: e})
